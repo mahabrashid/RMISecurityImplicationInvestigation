fbf26f56411954f693140ca2087a2afe3f57209b
mahab <mabz@donotemail.me>
2014-01-05 18:51:06 +0000

    Initiating version control. bin directory is also commited as it has the rmic complied binary.

---
+++ src/rmiClient/HelloClient.java
@@ -0,0 +1,17 @@
+package rmiClient;
+
+public class HelloClient {
+	/**
+	   * Client program for the "Hello, world!" example.
+	   * @param argv The command line arguments which are ignored.
+	   */
+	  public static void main (String[] argv) {
+	    try {
+	      HelloInterface hello = 
+	        (HelloInterface) Naming.lookup ("//ortles.ccs.neu.edu/Hello");
+	      System.out.println (hello.say());
+	    } catch (Exception e) {
+	      System.out.println ("HelloClient exception: " + e);
+	    }
+	  }
+}
